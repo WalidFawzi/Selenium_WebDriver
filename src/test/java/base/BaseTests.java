50eaaaf858a5f738a1b3a4f91dff6255638cf5f8
WalidFawzi <w.elbastawisi@diyarme.com>
2022-06-21 09:21:39 +0300

    Screenshots functions

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -2,10 +2,20 @@ package base;
 
 import Pages.HomePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+
+import java.awt.desktop.FilesEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class BaseTests {
 
@@ -24,6 +34,20 @@ public class BaseTests {
     @AfterClass
     public void tearDown() {
         driver.quit();
+    }
+
+    @AfterMethod
+    public void recordFailure(ITestResult result) {
+        if(ITestResult.FAILURE == result.getStatus()){
+            var camera = (TakesScreenshot) driver;
+            File screenshot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenshot, new File("resources/screenshots/" + result.getName() + ".png"),);
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
 }
